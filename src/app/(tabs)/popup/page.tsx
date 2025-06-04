d5282d151e97e27426e5fcbb3399eeb8da554d96
dev-hyeonmin <dev.hyeonmin@gmail.com>
2025-06-04 12:54:53 +0900

    refactor: restructure popup components
    - PopupItem.tsx 제거 후 PopupListItem.tsx로 재구성
    - popup 관련 컴포넌트들을 _components 폴더로 이동
    - PopupList, PopupListItem, PopupPreview 컴포넌트 분리
    - PageFooter 컴포넌트 추가

--- src/app/(tabs)/popup/page.tsx
+++ src/app/(tabs)/popup/page.tsx
@@ -1,46 +1,20 @@
 import PageTitle from '@/components/PageTitle';
-import PopupItem from '@/app/(tabs)/popup/PopupItem';
-import PopupPreview, { PopupPreviewProvider } from '@/app/(tabs)/popup/PopupPreview';
-import { getPopups } from './actions';
 import Link from 'next/link';
-import { Smile } from 'lucide-react';
-import { EmptyState } from '@/components/EmptyState';
+import PopupList from '@/app/(tabs)/popup/_components/PopupList';
+import PageFooter from '@/components/PageFooter';
 
 export default async function Popup() {
-  const popups = await getPopups();
   return (
     <div className="flex flex-col">
       <PageTitle title="Popup" subTitle="팝업" />
 
-      <PopupPreviewProvider>
-        {/* LIST */}
-        <div className="space-y-4">
-          {popups.map((popup) => (
-            <PopupItem key={`popup-${popup.id}`} {...popup} />
-          ))}
-        </div>
+      <PopupList />
 
-        <PopupPreview />
-      </PopupPreviewProvider>
-
-      {/* 빈 리스트 */}
-      {popups.length === 0 && (
-        <EmptyState
-          icon={Smile}
-          title="📌 등록된 팝업이 없어요"
-          description="지금 하나 추가해볼까요?"
-        />
-      )}
-
-      {/* 하단 고정 메뉴 */}
-      <div className="fixed right-0 bottom-0 left-64 flex justify-end border-t border-gray-200 bg-white px-12 py-4">
-        <Link
-          href={'/popup/add'}
-          className="rounded-lg bg-blue-700 px-8 py-3 text-white hover:bg-blue-600"
-        >
+      <PageFooter>
+        <Link href="/popup/add" className="primary-button">
           팝업 추가
         </Link>
-      </div>
+      </PageFooter>
     </div>
   );
 }
