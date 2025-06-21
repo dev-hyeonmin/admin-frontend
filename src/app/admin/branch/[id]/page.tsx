3ac3ea58392d5c968964fe2a0639224829f8b8bd
dev-hyeonmin <dev.hyeonmin@gmail.com>
2025-06-21 13:50:22 +0900

    fix: next async warning

--- src/app/admin/branch/[id]/page.tsx
+++ src/app/admin/branch/[id]/page.tsx
@@ -2,8 +2,10 @@ import BranchUpsertPage from '@/app/admin/branch/_components/BranchUpsertPage';
 import { getBranch } from '@/app/admin/branch/action';
 import { notFound } from 'next/navigation';
 
-export default async function Page({ params }: { params: { id: string } }) {
-  const id = Number(params.id);
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+  const resolvedParams = await params;
+  const id = Number(resolvedParams.id);
+
   const branch = await getBranch(Number(id));
 
   if (!branch) {
